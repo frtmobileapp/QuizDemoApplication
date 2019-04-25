730283a6ede29615e9c85779a7df9674af03ead0
Young Yang <yy949177@nd.com.cn>
2019-04-25 14:20:01 +0800

    System.out.println

--- app/src/main/java/cn/edu/fr/demo/quiz/app/MainActivity.java
+++ app/src/main/java/cn/edu/fr/demo/quiz/app/MainActivity.java
@@ -19,6 +19,7 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+        System.out.println("Hello World");
 
         FloatingActionButton fab = findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
